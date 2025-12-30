35a9e14f2623cbe51027a0bb26df99276063fabe
Soubhagyasw <soubhagyarswain377@gmail.com>
2025-12-30 22:29:37 +0530

    update

---
+++ JenkinWebApp1/src/main/java/com/nt/JenkinWebApp1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinWebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinWebApp1Application.class, args);
+	}
+
+}
